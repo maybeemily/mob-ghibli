99cc3f918671b17c98e5b4ab52b175e281c64ea0
emily baier <emilyrbaier@gmail.com>
2019-03-04 11:32:33 -0800

    started test, added preliminary styling

--- tests/tests.js
+++ tests/tests.js
@@ -1,1 +1,17 @@
 import './html-equal.js';
+
+const test = QUnit.test;
+
+function makeListTemplate(film) {
+    
+}
+
+test('list template will match HTML', assert => {
+    //arrange
+    const html = ` <li><a href="films.html">Castle In The Sky</a> <span>(1986)</span></li>`;
+    //act
+    const result = makeListTemplate(film);
+    //assert
+    assert.htmlEqual(result, ` <li><a href="films.html">Castle In The Sky</a> <span>(1986)</span></li>`);
+
+});
